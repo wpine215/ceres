43828ccf7ccd9742f7316beeb023de52db0485e4
wpine215 <wpine215@gmail.com>
2018-10-13 06:57:42 -0400

    Performed database migration

--- app/models.py
+++ app/models.py
@@ -1,16 +1,32 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 from app import db
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), index=True, unique=True)
+	email = db.Column(db.String(120), index=True, unique=True)
+	password_hash = db.Column(db.String(128))
+	fridges = db.relationship('Fridge', backref='owner', lazy='dynamic')
+
+	def set_password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	def check_password(self, password):
+		return check_password_hash(self.password_hash, password)
 
 class Fridge(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	item = db.Column(db.String(64))
+	quantity = db.Column(db.Integer)
+	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+	expiration = db.Column(db.DateTime, index=True)
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
+class Reference(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	item = db.Column(db.String(64), index=True, unique=True)
+	optimal = db.Column(db.String(32), index=False)
+	freezer = db.Column(db.Integer)
+	fridge = db.Column(db.Integer)
+	roomtemp = db.Column(db.Integer)
