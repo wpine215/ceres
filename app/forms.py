438017cc1d6c7777131988d9e9935add650f005f
wpine215 <wpine215@gmail.com>
2018-10-13 23:43:55 -0400

    Added a bunch of stuff

--- app/forms.py
+++ app/forms.py
@@ -3,14 +3,6 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, Selec
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from app.models import User
 
-choices = [{'dairy','Dairy'},
-		   {'fruit','Fruit'},
-		   {'vegetable', 'Vegetable'},
-		   {'meat', 'Meat'},
-	       {'grains', 'Grains'},
-		   {'seafood', 'Seafood'},
-		   {'beverage', 'Beverage'}]
-
 class LoginForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
@@ -35,8 +27,7 @@ class RegistrationForm(FlaskForm):
 			raise ValidationError('Please use a different e-mail.')
 
 class ItemForm(FlaskForm):
-	type = SelectField('Type', choices=choices)
-	item = SelectField('Item', choices=choices)
+	item = StringField('Item', validators=[DataRequired()])
 	quant = IntegerField('Quantity', validators=[DataRequired()])
 	# Add date picker here??
 	submit = SubmitField('Add item')
--- app/routes.py
+++ app/routes.py
@@ -1,41 +1,38 @@
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, render_template_string, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, ItemForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Fridge, Reference
 from datetime import datetime, timedelta
+from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
+from binascii import a2b_base64
+import json
+
+cfapp = ClarifaiApp(api_key='4f436d3877f148a7bf8ec1c680a44233')
+model = cfapp.models.get('food-items-v1.0')
 
 @app.route('/')
-@login_required
 def index():
+	return render_template('index.html')
+
+@app.route('/view')
+@login_required
+def view():
 	return render_template('view.html', title='View Items', fridges=Fridge.query.filter_by(user_id=current_user.get_id()).order_by(Fridge.expiration))
 
 @app.route('/add', methods=['GET', 'POST'])
 @login_required
 def add():
 	form = ItemForm()
-	form.type.choices = [{'dairy','Dairy'},
-		   {'fruit','Fruit'},
-		   {'vegetable', 'Vegetable'},
-		   {'meat', 'Meat'},
-	       {'grains', 'Grains'},
-		   {'seafood', 'Seafood'},
-		   {'beverage', 'Beverage'}]
-	form.item.choices = [{'dairy','Dairy'},
-		   {'fruit','Fruit'},
-		   {'vegetable', 'Vegetable'},
-		   {'meat', 'Meat'},
-	       {'grains', 'Grains'},
-		   {'seafood', 'Seafood'},
-		   {'beverage', 'Beverage'}]
-
 	if form.validate_on_submit():
+		# Calls to Reference table deactivated until it is populated
 		# ref_item = Reference.query.filter_by(item=form.item.data).first()
 		item = Fridge(user_id=current_user.get_id(),
 					  item=form.item.data,
 					  quantity=form.quant.data,
-					  expiration=datetime.now()-timedelta(days=5)#ref_item.roomtemp)
+					  expiration=datetime.now()+timedelta(days=5)#ref_item.roomtemp)
 				     )
 		db.session.add(item)
 		db.session.commit()
@@ -80,6 +77,31 @@ def register():
 		return redirect(url_for('login'))
 	return render_template('register.html', title='Register', form=form)
 
-@app.route('/webcam')
+@app.route('/addbyimg')
+@login_required
 def webcam():
 	return render_template('webcam.html')
+
+@app.route('/ai', methods=['POST'])
+def ai():
+	#if request.form['img'] is None:
+	#	return redirect(url_for('webcam'))
+	idata = request.form['img'][22:]
+	bdata = a2b_base64(idata)
+	with open('temp.png','wb') as fo:
+   		fo.write(bdata)
+
+	image = ClImage(file_obj=open('temp.png', 'rb'))
+	response = model.predict([image])
+	concept = response['outputs'][0]['data']['concepts'][0]['name']
+	return render_template_string("{{ data }}", data=concept)
+
+@app.route('/delete', methods=['GET', 'POST'])
+def delete():
+	itemID = request.form['id']
+	Fridge.query.filter_by(id=itemID).delete()
+	db.session.commit()
+	return render_template_string("success")
+	
+
+
